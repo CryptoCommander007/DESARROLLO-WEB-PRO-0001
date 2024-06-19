2325645041d1e596dc861c69a3d4e0dcf0c283af
CryptoCommander007 <rivantped@gmail.com>
2024-06-19 06:36:25 -0400

    DESARROLLO-WEB-PRO-0001

---
+++ run_commands.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "DESARROLLO-WEB-PRO-0001"
+git branch -M main
+git remote add origin https://github.com/CryptoCommander007/DESARROLLO-WEB-PRO-0001.git
+git push -u origin main
